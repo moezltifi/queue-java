03a7f01bc3f4c309d5db486fbe5da60fd6f7d08f
moezl <moez.mltifi@gmail.com>
2024-09-17 18:08:52 +0100

    ArrayQueue implement

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import queues.Employee;
+import queues.arrayQueue.ArrayQueue;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("ArrayQueue");
+        ArrayQueue queue = new ArrayQueue(10);
+        queue.add(new Employee("Moez", "one",123));
+        queue.add(new Employee("Mohamed", "two",23));
+        queue.add(new Employee("Ahmed", "three",723));
+        queue.add(new Employee("Jhon", "four",923));
+
+        queue.printQueue();
+        System.out.println("remove (): " + queue.remove());
+        System.out.println("size (): " + queue.size());
+        queue.remove();
+        System.out.println("size (): " + queue.size());
+        System.out.println("peek (): " + queue.peek());
+        queue.printQueue();
+
+
     }
 }
