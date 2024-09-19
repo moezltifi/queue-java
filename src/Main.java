cf07f5c4569b720edfb0397598776e88097a5bac
moezl <moez.mltifi@gmail.com>
2024-09-19 11:40:17 +0100

    CircularQueue implement

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import queues.Employee;
 import queues.arrayQueue.ArrayQueue;
+import queues.circularQueue.CircularQueue;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,6 +18,26 @@ public class Main {
         System.out.println("size (): " + queue.size());
         System.out.println("peek (): " + queue.peek());
         queue.printQueue();
+
+
+        System.out.println("CircularQueue");
+        CircularQueue circularQueue = new CircularQueue(10);
+        circularQueue.add(new Employee("Moez", "one",123));
+        circularQueue.add(new Employee("Mohamed", "two",23));
+        circularQueue.add(new Employee("Ahmed", "three",723));
+        circularQueue.add(new Employee("Jhon", "four",923));
+
+        circularQueue.printQueue();
+        System.out.println("remove (): " + circularQueue.remove());
+        circularQueue.add(new Employee("jean", "five",123));
+        System.out.println("size (): " + circularQueue.size());
+        System.out.println("remove (): " + circularQueue.remove());
+        System.out.println("size (): " + circularQueue.size());
+        circularQueue.add(new Employee("bill", "end",123));
+        System.out.println("peek (): " + circularQueue.peek());
+        circularQueue.printQueue();
+
+
 
 
     }
